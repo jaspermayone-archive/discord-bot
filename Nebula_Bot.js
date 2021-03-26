245d5ef19e50bfa85321c67c4f1412be814ef70e
j-dogcoder <j.dogcoder@gmail.com>
2021-03-26 16:22:01 -0400

    More work

--- Nebula_Bot.js
+++ Nebula_Bot.js
@@ -1,15 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
+
 
 client.on("ready", () => {
   console.log("Bot online and Ready!");
 });
 
 // Set the prefix
-const prefix = "!";
+let prefix = "!";
 client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
