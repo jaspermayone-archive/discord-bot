b5dd37b9c6f7467449316b4706cacdbe983a3e54
j-dogcoder <j.dogcoder@gmail.com>
2021-03-26 21:28:29 -0400

    more work

--- commands/github.js
+++ commands/github.js
@@ -2,6 +2,9 @@ module.exports = {
     name: 'github',
     description: "sends github link",
     execute(message, args){
+
+        if(message.member.roles.cache.has('825176015562997781')){
         message.channel.send('https://github.com/Pluto-Ai');
     }
+}
 }
