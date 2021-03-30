1605bda49be5692d9f533b3943b83b0dbe72a9d5
j-dogcoder <j.dogcoder@gmail.com>
2021-03-30 14:34:11 -0400

    Adds invite and support commands in the Resources folder

---
+++ commands/Resources/invite.js
@@ -0,0 +1,9 @@
+//This is the invite command. The invite command sends the bot invite. 
+
+module.exports = {
+    name: 'invite',
+    description: "sends the bot invite link",
+    execute(message, args, Discord) {
+        message.channel.send('You can invite Heptagram to your server at https://discord.com/oauth2/authorize?client_id=783073095036043274&permissions=8&scope=bot');
+    }
+}
--- main.js
+++ main.js
@@ -38,7 +38,7 @@ client.on('message', message => {
 		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply('there was an error trying to execute that command! Please contact a developer in our support server.');
 	}
 });
 
