91a54816861b4351c988cd6cc3ea4130d5ab782e
j-dogcoder <j.dogcoder@gmail.com>
2021-03-31 09:28:25 -0400

    Working on bug squashing.

--- commands/Utilitys/ping.js
+++ commands/Utilitys/ping.js
@@ -1,10 +1,13 @@
 //This is the ping command. The ping command currently just returns pong.
 // We would like to have it give the bot ping in the future.
 
+  
 module.exports = {
-    name: 'ping',
-    description: "this is a ping command!",
-    execute({ message }) {
-        message.channel.send('pong!');
-    }
-}
+	name: 'ping',
+	description: 'Ping!',
+	cooldown: 5,
+    args: false,
+	execute(message) {
+		message.channel.send('Pong.');
+	},
+};
