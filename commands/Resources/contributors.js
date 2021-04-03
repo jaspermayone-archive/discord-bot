d7824788802d2ad39d4c38148f0100866cba9867
thinkverse <hallberg.kim@gmail.com>
2021-04-04 01:18:11 +0200

    Create a command to list contributors

---
+++ commands/Resources/contributors.js
@@ -0,0 +1,43 @@
+const https = require('https');
+
+const fetchContributors = new Promise((resolve, reject) => {
+  https.get({
+    hostname: 'api.github.com',
+    path: '/repos/Heptagram-Bot/Heptagram/contributors',
+    headers: { 'User-Agent': 'Heptagram-Bot/1.0' }
+  }, response => {
+    response.setEncoding("utf8");
+    let body = "";
+
+    response.on("data", data => body += data);
+
+    response.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    response.on('error', error => reject(error));
+  }).on('error', error => reject(error));
+});
+
+module.exports = {
+  name: 'contributors',
+  description: "Show current contributors for Heptagram",
+  execute({ message }) {
+    fetchContributors.then(contributors => {
+      let listOfContributors = `Heptagram\'s contributors are awesome, here is a list of them!\n\n`;
+        
+        contributors
+          .filter(contributor => ! contributor.login.includes('[bot]') || contributor.type === 'User')
+          .map(contributor => listOfContributors += `+ ${contributor.login} - ${contributor.contributions} Contributions.\n`);
+        
+        message.channel.send(listOfContributors);
+    }).catch(error => {
+      message.channel.send(`Something went wrong: ${error}`);
+      console.error(error);
+    });
+  }
+}
