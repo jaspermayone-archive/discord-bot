54ca8c37b9c67b0a8c71006884c2beeeed53dbf0
j-dogcoder <j.dogcoder@gmail.com>
2021-04-07 12:02:20 -0400

    Changes to bot console.log

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
   .on("warn", info => client.logger.warn(info))
 
 client.on("ready", async () => {
+	console.log("Starting Heptagram\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
 	console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 	client.user.setStatus('online');
 	client.user.setActivity('!support', { type: 'PLAYING' })
