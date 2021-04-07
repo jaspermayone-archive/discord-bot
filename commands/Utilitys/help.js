c32cf6aa447b09cab90ca6e189df5c3347e6504e
Christopher Lo <lochristopher9@gmail.com>
2021-04-07 10:42:26 -0400

    Add help command (filtering admin commands as well). (#61)
    Co-authored-by: Jasper Mayone <j.dogcoder@gmail.com>

--- commands/Utilitys/help.js
+++ commands/Utilitys/help.js
@@ -7,7 +7,13 @@ module.exports = {
     usage: ['command-name'],
     cooldown: 5,
 
-    execute( {message, args, roles} ) {
+  execute( {message, args, roles} ) {
+        const data = [];
+        const { commands } = message.client;
+        if (!args) {
+            return message.reply('no arguments present!');
+        }        
+        if (!args.length) {
             data.push('Here\'s a list of all my commands:');            
             var commandNames = commands.map(command => command.name);
             // filter out admin commands if not an admin
@@ -19,6 +25,9 @@ module.exports = {
                 });                
             }
             data.push(commandNames.join(', '));
+            data.push('Here\'s a list of all my commands:');
+            data.push(commands.map(command => command.name).join(', '));
+
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })
