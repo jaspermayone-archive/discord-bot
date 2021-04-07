b8ee79c6bee715e098b6574269e12a7d07084d3a
ting-c <ting.cheong31@gmail.com>
2021-04-07 17:06:50 +0100

    Change to a more stable API and update logic to prevent spamming

--- commands/Fun/joke.js
+++ commands/Fun/joke.js
@@ -1,26 +1,44 @@
 // For generating a random joke fetch from an API
-// Checkout https://jokeapi.dev/#endpoints for more categories
-
 const fetch = require("node-fetch");
 
-const category = "any";
-const URL = `https://v2.jokeapi.dev/joke/${category}`; // 120 request per minute limit
+const URL = "https://official-joke-api.appspot.com/random_joke"; // 120 request per minute limit
 
 function fetchJoke() {
 	return fetch(URL).then((res) => res.json());
 }
 
+let blocked = false;
+
 module.exports = {
 	name: "joke",
 	description: "fetch joke from API and send",
 	execute({ message, args, roles }) {
+		// To prevent user spamming the same command
+		if (blocked) {
+			message.channel.send(
+				"Please wait until the joke finishes before using this command again."
+			);
+			return
+		};
 		fetchJoke()
-			.then(({ setup, delivery }) => {
+			.then(response => {
+				const { setup, punchline } = response;
+				if (!setup || !punchline) {
+					message.channel.send("Please wait a few seconds and try again.");
+					return
+				}
+				blocked = true;
 				message.channel.send(setup);
-				// wait 3 seconds before sending the delivery
-				setTimeout(() => {
-					message.channel.send(delivery);
-				}, 3000);
+				return new Promise(function(resolve, reject) {
+					// wait 3 seconds before sending the punchline
+					setTimeout(() => {
+						message.channel.send(punchline);
+						resolve();
+					}, 3000);
+				})
+			})
+			.then(() => {
+				blocked = false;
 			})
 			.catch((err) => console.warn(err));
 	},
