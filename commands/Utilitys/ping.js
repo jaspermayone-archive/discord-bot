fc269c9da182f400bb94130e6301e8b0cb2757d9
j-dogcoder <j.dogcoder@gmail.com>
2021-04-14 13:10:33 -0400

    Fixes issue where API Latency is reported back as NaN

--- commands/Utilitys/ping.js
+++ commands/Utilitys/ping.js
@@ -2,13 +2,13 @@
 // We would like to have it give the bot ping in the future.
 
 module.exports = {
-    name: 'ping',
-    guildOnly: false,
-    description: "this is a ping command!",
-    execute({ discord, client, message, roles }) {
-            //        message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
+  name: 'ping',
+  guildOnly: false,
+  description: "this is a ping command!",
+  execute({ discord, client, message, roles }) {
+    //        message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
 
-               let ping = message.createdTimestamp - message.createdTimestamp;
-              message.channel.send(`Bot Latency: \`${ping}\`, API Latency: \`${Math.round(client.ping)}\` Websocket Heartbeat: \`${client.ws.ping}ms.\``);
-            }
+    let ping = message.createdTimestamp - message.createdTimestamp;
+    message.channel.send(`Bot Latency: \`${ping}ms\`, API Latency: \`${Math.round(message.client.ws.ping)}ms\` Websocket Heartbeat: \`${client.ws.ping}ms.\``);
+  }
 }
