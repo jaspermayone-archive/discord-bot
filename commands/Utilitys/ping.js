8e9197d65677521fe87b9cfee03dab4eb2784f28
j-dogcoder <j.dogcoder@gmail.com>
2021-04-15 19:57:47 -0400

    Misc work

--- commands/Utilitys/ping.js
+++ commands/Utilitys/ping.js
@@ -3,8 +3,6 @@ module.exports = {
   guildOnly: false,
   description: "returns bot ping.",
   execute({ discord, client, message, roles }) {
-    //        message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
-
     let ping = message.createdTimestamp - message.createdTimestamp;
     message.channel.send(`Bot Latency: \`${ping}ms\`, API Latency: \`${Math.round(message.client.ws.ping)}ms\` Websocket Heartbeat: \`${client.ws.ping}ms.\``);
   }
