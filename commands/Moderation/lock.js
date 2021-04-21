3f00715da2d328c5cd511fa9118586218989e02e
thinkverse <hallberg.kim@gmail.com>
2021-04-21 14:21:37 +0200

    Fix lock command

--- commands/Moderation/lock.js
+++ commands/Moderation/lock.js
@@ -5,22 +5,25 @@ module.exports = {
     category: "moderation",
     description: "Locks a Channel",
     guildOnly: true,
-
-        execute: async (client, message, args) => {
-            if (message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS') && message.member.roles.cache.has(roles.admin)) {
-                return message.channel.send("You don't have enough Permissions")
-            }
-            message.channel.overwritePermissions([
-                {
-                    id: message.guild.id,
-                    deny: ['SEND_MESSAGES'],
-                },
-            ]);
-            const embed = new Discord.MessageEmbed()
-                .setTitle("Channel Updates")
-                .setDescription(`ðŸ”’ ${message.channel} has been Locked`)
-                .setColor("RANDOM");
-            await message.channel.send(embed);
-            message.delete();
+    execute: async ({ message, roles }) => {
+        if (! (message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS') && message.member.roles.cache.has(roles.admin))) {
+            return message.channel.send("You don't have enough Permissions");
         }
+
+        message.channel.overwritePermissions([
+            {
+                id: message.guild.id,
+                deny: ['SEND_MESSAGES'],
+            },
+        ]);
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Channel Updates")
+            .setDescription(`${message.channel} has been Locked`)
+            .setColor("RANDOM");
+
+        await message.channel.send(embed);
+
+        message.delete();
     }
+}
