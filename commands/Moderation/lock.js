8b6acf44ac7089e649b8e1fea6d29dfaa5c9549a
j-dogcoder <j.dogcoder@gmail.com>
2021-04-21 08:07:11 -0400

    :bug: Starts bug #100

--- commands/Moderation/lock.js
+++ commands/Moderation/lock.js
@@ -5,21 +5,22 @@ module.exports = {
     category: "moderation",
     description: "Locks a Channel",
     guildOnly: true,
-}
-    run: async (client, message, args) => {
-        if (!message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS')&& message.member.roles.cache.has(roles.admin)) {
-            return message.channel.send("You don't have enough Permissions")
+
+        execute: async (client, message, args) => {
+            if (message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS') && message.member.roles.cache.has(roles.admin)) {
+                return message.channel.send("You don't have enough Permissions")
+            }
+            message.channel.overwritePermissions([
+                {
+                    id: message.guild.id,
+                    deny: ['SEND_MESSAGES'],
+                },
+            ]);
+            const embed = new Discord.MessageEmbed()
+                .setTitle("Channel Updates")
+                .setDescription(`ðŸ”’ ${message.channel} has been Locked`)
+                .setColor("RANDOM");
+            await message.channel.send(embed);
+            message.delete();
         }
-        message.channel.overwritePermissions([
-            {
-                id: message.guild.id,
-                deny: ['SEND_MESSAGES'],
-            },
-        ]);
-        const embed = new Discord.MessageEmbed()
-            .setTitle("Channel Updates")
-            .setDescription(`ðŸ”’ ${message.channel} has been Locked`)
-            .setColor("RANDOM");
-        await message.channel.send(embed);
-        message.delete();
-    };
+    }
