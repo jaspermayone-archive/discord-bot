c4cc3cd4071f9f73f25148929e07174107e9d35b
j-dogcoder <j.dogcoder@gmail.com>
2021-04-21 10:14:56 -0400

    :sparkles: New info command and info command catagory.

--- commands/Info/userinfo.js
+++ commands/Info/userinfo.js
@@ -2,7 +2,7 @@
 module.exports = {
     name: 'userinfo',
     description: "gives info about user.",
-    category: "Utilitys",
+    category: "Info",
     
     execute({ message, args, roles }) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
