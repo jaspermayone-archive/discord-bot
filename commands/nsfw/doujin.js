43dc244bf85f270319070ae7c9978f8d85bc2eb4
Kim Hallberg <hallberg.kim@gmail.com>
2021-04-22 14:44:35 +0200

    Clean up NSFW commands. (#107)
    * :bug: Starts bug #100
    
    * Fix lock command
    
    * Clean up NSFW commands
    Remove unused import in NSFW commands
    Fix panty NSFW command using wrong name AND function
    
    Co-authored-by: j-dogcoder <j.dogcoder@gmail.com>

--- commands/nsfw/doujin.js
+++ commands/nsfw/doujin.js
@@ -1,21 +1,14 @@
-const discord = require("discord.js");
 const akaneko = require('akaneko');
 
 module.exports = {
   name: "doujin",
   category: "nsfw",
   description: "NSFW Command",
-  
-  execute: async ({ Discord, client, message, args }) => { 
-
-    if(message.channel.nsfw) {
-      
-      return message.channel.send(await akaneko.nsfw.doujin());
-
-    } else {
-    
-      return message.channel.send("This channel dosen't support nsfw content")
-      
+  execute: async ({ message }) => {
+    if (! message.channel.nsfw) {
+      return message.channel.send("This channel dosen't support NSFW content.");
     }
+
+    return message.channel.send(await akaneko.nsfw.doujin());
   }
 }
--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -1,21 +1,14 @@
-const discord = require("discord.js");
 const akaneko = require('akaneko');
 
 module.exports = {
   name: "hentai",
   category: "nsfw",
   description: "NSFW Command",
-     
-  execute: async ({ Discord, client, message, args }) => { 
-    
-    if(message.channel.nsfw) {
-      
-      return message.channel.send(await akaneko.nsfw.hentai());
-
-    } else {
-    
-      return message.channel.send("This channel dosen't support nsfw content")
-      
+  execute: async ({ message }) => {
+    if (! message.channel.nsfw) {
+      return message.channel.send("This channel dosen't support NSFW content.");
     }
+
+    return message.channel.send(await akaneko.nsfw.hentai());
   }
 }
