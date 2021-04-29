a2cd4c3dd5cda789b84236597c540cd1a4f45922
j-dogcoder <j.dogcoder@gmail.com>
2021-04-29 07:15:26 -0400

    :sparkles: Urban Dictionary Command

--- commands/Resources/invite.js
+++ commands/Resources/invite.js
@@ -5,6 +5,7 @@ module.exports = {
     category: "Resources",
 
     execute({ message, roles }) {
-        message.channel.send('You can invite Heptagram to your server at https://discord.com/oauth2/authorize?client_id=783073095036043274&permissions=8&scope=bot');
+       // message.channel.send('You can invite Heptagram to your server at https://discord.com/oauth2/authorize?client_id=783073095036043274&permissions=8&scope=bot');
+    message.channel.send('Heptagram is not yet avaible to be invited to servers.')
     }
 }
