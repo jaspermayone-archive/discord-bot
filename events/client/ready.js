15bf05fedbb68bde116b6870db7efa7d6f32c317
j-dogcoder <j.dogcoder@gmail.com>
2021-05-09 14:58:10 -0400

    :package: Works on start commands

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,7 +5,7 @@ const chalk = require('chalk');
 module.exports = async (Discord, client, args) => {
 	
 	console.log(chalk.magenta("Starting Heptagram\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version));
-	console.log(chalk.cyan(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`));
+	console.log(chalk.green(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`));
 	
 	client.user.setStatus('online');
     client.user.setActivity(`${prefix}help`, { type: "LISTENING" })
