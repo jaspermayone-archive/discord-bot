b18aba44f9a57af4b9ffd7b679ddc506197a5ad5
j-dogcoder <j.dogcoder@gmail.com>
2021-05-09 14:44:44 -0400

    :egg: Terminal now has a colorful start prosses

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,13 +1,15 @@
 const dbLogging = require("../dbLogging");
 const { prefix, token, roles, MongoDB, serverId } = require('../../config.json');
+const chalk = require('chalk');
 
 module.exports = async (Discord, client, args) => {
-	console.log("Starting Heptagram\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
-	console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
+	
+	console.log(chalk.magenta("Starting Heptagram\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version));
+	console.log(chalk.cyan(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`));
 	
 	client.user.setStatus('online');
     client.user.setActivity(`${prefix}help`, { type: "LISTENING" })
-		.then(presence => console.log(`Activity set to ${presence.activities[0].name}.`))
+		.then(presence => console.log(chalk.cyanBright(`Activity set to ${presence.activities[0].name}.`)))
 		.catch(console.error);
 
 	const { shard, voice, users, guilds, channels, presence, user, readyAt, commands } = client;
