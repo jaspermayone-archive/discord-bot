a69e6813b44db902790915f65106c49650654ed0
j-dogcoder <j.dogcoder@gmail.com>
2021-05-18 11:59:58 -0400

    :adhesive_bandage: Fixes user command

--- commands/Info/user.js
+++ commands/Info/user.js
@@ -1,5 +1,7 @@
 
 const { prefix, token, roles, MongoDB, serverId, colors } = require('../../config.json');
+const moment = require('moment');
+const Discord = require('discord.js')
 
 module.exports = {
     name: 'user',
@@ -7,23 +9,10 @@ module.exports = {
     category: "Info",
 
     execute({ message, Discord, args }) {
-        let user = message.mentions.users.first() || message.author;
-
-        const joinDiscord = moment(user.createdAt).format('llll');
-        const joinServer = moment(user.joinedAt).format('llll');
-        
         const embed = new Discord.MessageEmbed()
-        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        .setTitle("User Info :")
-        .setThumbnail(`${user.displayAvatarURL}`)
-        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-        .addField('Status:', user.presence.status, true)
-        .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
-        .setFooter(`ID: ${user.id}`)
-        .setTimestamp()
+        .setTitle(`User Info for ${message.author.username}`)
         .setColor(colors.heptagram)
         .setDescription(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`)
-        
-        message.channel.send(embed);
+    message.channel.send(embed);
 }
 }
