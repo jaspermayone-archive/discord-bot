ae0af7828a5c78b1dc27442e36ff437ba7c66258
j-dogcoder <j.dogcoder@gmail.com>
2021-05-18 10:16:50 -0400

    :truck: Renames info commands

--- commands/Info/info.js
+++ commands/Info/info.js
@@ -1,11 +1,33 @@
+
+const { prefix, token, roles, MongoDB, serverId, colors } = require('../../config.json');
+
 module.exports = {
-    name: 'info',
+    name: 'botinfo',
     description: "Displays info about bot.",
     guildOnly: true,
-    category: "Info",
+    category: "Bot Info",
 
-   execute ({ discord, client, message, roles }) {
-        message.channel.send('This is the Heptagram discord bot. Heptagam has a variety of info commands.');
-        message.channel.send(`Run \`!serverinfo\` for server statistics, \`!userinfo\` for info about a user, and \`!botinfo\` for info about this bot. `)
+    execute({ Discord, client, message, roles }) {
+        let ping = message.createdTimestamp - message.createdTimestamp;
+
+        let days = Math.floor(client.uptime / 86400000);
+        let hours = Math.floor(client.uptime / 3600000) % 24;
+        let minutes = Math.floor(client.uptime / 60000) % 60;
+        let seconds = Math.floor(client.uptime / 1000) % 60;
+
+        message.channel.send('This is the Heptagram discord bot. Heptagram is the open-source multipurpose discord bot with the goal to be the single needed bot for any server.');
+
+        message.channel.send(`**You can find out more about Heptagram in our support server or on our GitHub Repository.**`);
+        message.channel.send(`Run \`${prefix}repo\` for our repo, or \`${prefix}server\` for a link to our support server. `),
+
+
+        message.channel.send(`**Heptagram will now display a variety of statistics.**`);
+
+        message.channel.send(`*Here is Heptagram's ping and uptime:*`);
+        message.channel.send(`Bot Latency: \`${ping}ms\`, API Latency: \`${Math.round(message.client.ws.ping)}ms\` Websocket Heartbeat: \`${client.ws.ping}ms.\``);
+        message.channel.send(`Bot Uptime: \`${days}d ${hours}h ${minutes}m ${seconds}s\``);
+
+        message.channel.send(`**Here is are some statistics unique to this server:**`);
+        message.channel.send(`*Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}*`);
     }
 }
