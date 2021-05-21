b5c8dbb21f183488484ba560ce036f78e8f10d27
j-dogcoder <j.dogcoder@gmail.com>
2021-05-21 14:17:10 -0400

    :children_crossing: Starts command cooldown work

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.events = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
 	require(`./handlers/${handler}`)({ client, Discord });
