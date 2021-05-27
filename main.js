506489e46d0f129a647e18f28238919c30ce3c35
j-dogcoder <j.dogcoder@gmail.com>
2021-05-27 14:24:46 -0400

    :lipstick: Final update to meanntion message

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ mongoose.connect((MongoDB), {
 client.on("message", async message => {
 	const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
 	if (message.content.match(prefixMention)) {
-	  return message.reply(`Hey there! Need some help? My commands can be accessed through my prefix. My prefix in this server is \`${prefix}\``);
+	  return message.reply(`Hey there! Need some help? My commands can be accessed through my prefix. My prefix in this server is \`${prefix}\`. You can use \`${prefix}help\` for a list of all my commands.`);
 	}
 })
 
