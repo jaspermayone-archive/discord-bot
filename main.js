d4841cbb5a60eea7a0ac2b5ac899f71d5fa3f7e1
j-dogcoder <j.dogcoder@gmail.com>
2021-05-27 14:23:24 -0400

    :children_crossing: Complete meantion restructure

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ mongoose.connect((MongoDB), {
 client.on("message", async message => {
 	const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
 	if (message.content.match(prefixMention)) {
-	  return message.reply(`My prefix in this server is \`${prefix}\``);
+	  return message.reply(`Hey there! Need some help? My commands can be accessed through my prefix. My prefix in this server is \`${prefix}\``);
 	}
 })
 
