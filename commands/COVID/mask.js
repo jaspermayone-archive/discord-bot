5c3c7d085a3ae1e3e8f39c57c40d7446c70c9b15
ting-c <ting.cheong31@gmail.com>
2021-06-01 11:14:13 +0100

    Add command cooldowns function

--- commands/COVID/mask.js
+++ commands/COVID/mask.js
@@ -1,14 +1,18 @@
 
 const { prefix, token, roles, MongoDB, serverId, Discord, colors } = require('../../config.json');
 const { MessageEmbed } = require("discord.js");
+const commandCooldowns = require('../../command-cooldowns');
 
 module.exports = {
     name: 'mask',
     guildOnly: false,
     description: "tells chat to put their mask on",
     category: "COVID",
+    cooldown: 5,
 
-    execute({ message, Discord, args }) {
+    execute({ message, Discord, client, args }) {
+        if (commandCooldowns({ name: this.name, cooldown: this.cooldown, message, Discord, client })) return;
+
         const embed = new Discord.MessageEmbed()
             .setTitle("COVID Police: 🚨")
             .setColor(colors.heptagram)
