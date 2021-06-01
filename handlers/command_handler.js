1c1fa6e8c933ceab91d3ddcbcb7a8c9733d8fa22
ting-c <ting.cheong31@gmail.com>
2021-06-01 08:09:41 +0100

    Add command cooldown

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -2,12 +2,31 @@ const fs = require('fs');
 
 module.exports = ({ client, Discord }) => {
 	const commandFolders = fs.readdirSync('./commands');
+	const { cooldowns } = client;
 
 	for (const folder of commandFolders) {
 		const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 		for (const file of commandFiles) {
 			const command = require(`../commands/${folder}/${file}`);
 			if (command.name) {
+				if (!cooldowns.has(command.name)) {
+					cooldowns.set(command.name, new Discord.Collection());
+				}
+				const now = Date.now();
+				const timestamps = cooldowns.get(command.name);
+				const cooldownAmount = (command.cooldown || 3) * 1000;
+
+				if (timestamps.has(message.author.id)) {
+					const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+					if (now < expirationTime) {
+						const timeLeft = (expirationTime - now) / 1000;
+						return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+					}
+				}
+
+				timestamps.set(message.author.id, now);
+				setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
 				client.commands.set(command.name, command);
 			} else {
 				continue;
