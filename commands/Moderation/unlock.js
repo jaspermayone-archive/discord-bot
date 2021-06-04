1ed2fb0a04bd70e43bc188c7c989fe679b65e9a0
thinkverse <hallberg.kim@gmail.com>
2021-06-04 21:37:40 +0200

    Add simple unlock command

---
+++ commands/Moderation/unlock.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "unlock",
+  category: "moderation",
+  description: "Unlock a Channel",
+  guildOnly: true,
+  execute: async ({ message, roles, Discord }) => {
+      if (! (message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS') && message.member.roles.cache.has(roles.admin))) {
+          return message.channel.send("You don't have enough Permissions");
+      }
+
+      message.channel.overwritePermissions([
+          {
+              id: message.guild.id,
+              allow: ['SEND_MESSAGES'],
+          },
+      ]);
+
+      const embed = new Discord.MessageEmbed()
+          .setTitle("Channel Updates")
+          .setDescription(`${message.channel} has been unlocked`)
+          .setColor("RANDOM");
+
+      await message.channel.send(embed);
+
+      message.delete();
+  }
+};
