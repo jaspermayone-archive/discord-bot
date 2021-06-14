7c7e08e06a7a1e7bd17b09c2da50bf5c7e9f315d
j-dogcoder <j.dogcoder@gmail.com>
2021-06-14 12:24:37 -0400

    :children_crossing: Fixes inacurate info in info command

--- commands/Resources/info.js
+++ commands/Resources/info.js
@@ -14,7 +14,7 @@ module.exports = {
             .setColor(colors.heptagram)
             .setDescription(`This is the Heptagram discord bot. Heptagram is the open-source multipurpose discord bot with the goal to be the single needed bot for any server.`)
             .addFields(
-                { name: 'Bot Help:', value: `Run \`${prefix}repo\` for our repo, or \`${prefix}server\` for a link to our support server. `, inline: true },
+                { name: 'Bot Help:', value: `You can run \`${prefix}repo\` for our repo, \`${prefix}support\` for a link to our support server, or \`${prefix}server\` for stats and info about this server. `, inline: true },
                 { name: 'More info:', value: `You can find out more about Heptagram in our support server or on our GitHub Repository.`, inline: true },
             )
         message.channel.send(embed);
