ca9816ad6fb7cb4f2bc22617f5365cd77e33f817
j-dogcoder <j.dogcoder@gmail.com>
2021-06-14 11:45:55 -0400

    :bug: Fixes and improves user command.
    Closes #162

--- commands/Resources/user.js
+++ commands/Resources/user.js
@@ -1,4 +1,4 @@
-
+const moment= require('moment') 
 const { prefix, token, roles, MongoDB, serverId, colors } = require('../../config.json');
 
 module.exports = {
@@ -8,10 +8,30 @@ module.exports = {
     guildOnly: false,
 
     execute({ message, Discord, args }) {
+        
+        let user;
+        if (message.mentions.users.first()) {
+            user = message.mentions.users.first();
+        } else {
+            user = message.author;
+        }
+        
+        const member = message.guild.member(user);
+        
         const embed = new Discord.MessageEmbed()
-        .setTitle(`User Info for ${message.author.username}`)
-        .setColor(colors.heptagram)
-        .setDescription(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`)
-    message.channel.send(embed);
-}
+            .setColor(colors.heptagram)
+            .setThumbnail(message.author.avatarURL)
+            .addField(`${user.tag}`, `${user}`, true)
+            .addField("ID:", `${user.id}`, true)
+            .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+            .addField("Status:", `${user.presence.status}`, true)
+            .addField("In Server", message.guild.name, true)
+            .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+            .addField("Bot:", `${user.bot}`, true)
+            .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
+            .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+            .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+      
+            message.channel.send({embed});
+    }
 }
