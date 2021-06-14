b6a816f25db16c8d19c5728df23841b41e9784e0
j-dogcoder <j.dogcoder@gmail.com>
2021-06-14 11:52:35 -0400

    :pencil2: Fix typo inn support command

--- commands/Resources/support.js
+++ commands/Resources/support.js
@@ -6,9 +6,9 @@ module.exports = {
 
     execute({ message, roles }) {
         if (message.member.roles.cache.has(roles.admin)) {
-            message.channel.send('Need help with Heptigram? Join our discord server at https://discord.gg/HSupF99kpq');
+            message.channel.send('Need help with Heptagram? Join our discord server at https://discord.gg/HSupF99kpq');
         } else {
-            message.channel.send('Need help with Heptigram? Have someone with Admin permissions run this command');
+            message.channel.send('Need help with Heptagram? Have someone with Admin permissions run this command');
         }
     }
 }
