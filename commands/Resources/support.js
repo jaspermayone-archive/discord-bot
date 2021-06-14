3816f6dd1dc139cb02dddf6b1e107aaf2583d925
j-dogcoder <j.dogcoder@gmail.com>
2021-06-14 12:02:56 -0400

    :lipstick: Changes support command over to embeds

--- commands/Resources/support.js
+++ commands/Resources/support.js
@@ -1,14 +1,33 @@
+
+const { prefix, token, roles, MongoDB, serverId, colors } = require('../../config.json');
+
 module.exports = {
     name: 'support',
     guildOnly: true,
     description: "sends a link for the bot support server",
     category: "Resources",
 
-    execute({ message, roles }) {
+    execute({ message, Discord, roles }) {
+
         if (message.member.roles.cache.has(roles.admin)) {
-            message.channel.send('Need help with Heptagram? Join our discord server at https://discord.gg/HSupF99kpq');
+
+            const inviteEmbed = new Discord.MessageEmbed()
+                .setColor(colors.heptagram)
+                .setTitle('Need some help with the Heptagram Bot?')
+                .setDescription('Join our discord server at https://discord.gg/HSupF99kpq')
+                .setFooter('This is for Heptagram Bot help only. For server specific help, contact a Modarator or Admin.');
+
+            message.channel.send(inviteEmbed);
+
         } else {
-            message.channel.send('Need help with Heptagram? Have someone with Admin permissions run this command');
+
+            const userEmbed = new Discord.MessageEmbed()
+                .setColor(colors.heptagram)
+                .setTitle('Need some help with the Heptagram Bot?')
+                .setDescription('Need help with the Heptagram Bot? Have someone with Admin or Modarator permissions in this server run this command')
+                .setFooter('This is for Heptagram Bot help only. For server specific help, contact a Modarator or Admin.');
+
+            message.channel.send(userEmbed);
         }
     }
 }
