068bde6ea24f1fd155eb531e4d1e6856860216fc
j-dogcoder <j.dogcoder@gmail.com>
2021-06-14 21:43:36 -0400

    :art: and some more linting...

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-module.exports = ({ client, Discord }) => {
+module.exports = ({ client }) => {
 	const commandFolders = fs.readdirSync('./commands');
 
 	for (const folder of commandFolders) {
