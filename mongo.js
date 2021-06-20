db11db78383bfa813de7165fbb1d777b7272f546
j-dogcoder <j.dogcoder@gmail.com>
2021-06-19 20:16:41 -0400

    :card_file_box: Starts work on new database system.

---
+++ mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { MongoDB } = require('./config.json');
+
+module.exports = async () => {
+	await mongoose.connect(MongoDB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	});
+	return mongoose;
+};
