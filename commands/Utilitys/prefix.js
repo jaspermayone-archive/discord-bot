54d82e3f05610a07de48c81f5cb13a751a92a049
j-dogcoder <j.dogcoder@gmail.com>
2021-06-19 21:12:00 -0400

    :card_file_box: Does Database side to prefix command (Issue #182)

---
+++ commands/Utilitys/prefix.js
@@ -0,0 +1,47 @@
+const mongo = require('../../mongo');
+const prefixSchema = require('../../schemas/prefix-schema');
+
+module.exports = {
+	name: 'prefix',
+	description: 'configures the bot prefix for server.',
+	guildOnly: true,
+	category: 'Utilitys',
+
+	async execute({ message }) {
+		const { member, channel, content, guild } = message;
+
+		if (!member.hasPermission('ADMINISTRATOR')) {
+			channel.send('You do not have permission to change this servers prefix.');
+			return;
+		}
+
+		let text = content;
+
+		const split = text.split(' ');
+
+		if (split.length < 2) {
+			channel.send('Please specify the new prefix.');
+			return;
+		}
+
+		split.shift();
+
+		text = split.join(' ');
+
+		await mongo().then(async (mongoose) => {
+			try {
+				await prefixSchema.findOneAndUpdate({
+					_id: guild.id,
+				}, {
+					_id: guild.id,
+					prefix: text,
+				}, {
+					upsert: true,
+				});
+			}
+			finally {
+				mongoose.connection.close();
+			}
+		});
+	},
+};
---
+++ schemas/prefix-schema.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+const mongoose = require('mongoose');
+
+const reqString = {
+	type: String,
+	required: true,
+};
+
+const prefixSchema = mongoose.Schema({
+	_id: reqString,
+	prefix: reqString,
+});
+
+module.exports = mongoose.model('prefix', prefixSchema);
