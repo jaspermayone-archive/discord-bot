9cd400852572cd7f4a610db742e9277ce431e1ba
j-dogcoder <j.dogcoder@gmail.com>
2021-06-20 06:43:26 -0400

    :card_file_box: more database work for prefix command

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
 const Discord = require('discord.js');
 const chalk = require('chalk');
+
 const { prefix, token } = require('./config.json');
 const mongo = require('./mongo');
 
@@ -32,7 +32,7 @@ client.on('ready', async () => {
 client.on('message', async message => {
 	const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
 	if (message.content.match(prefixMention)) {
-	  return message.reply(`Hey there! Need some help? My commands can be accessed through my prefix. My prefix in this server is \`${prefix}\`. You can use \`${prefix}help\` for a list of all my commands.`);
+		return message.reply(`Hey there! Need some help? My commands can be accessed through my prefix. My prefix in this server is \`${prefix}\`. You can use \`${prefix}help\` for a list of all my commands.`);
 	}
 });
 
