f510d6c98d88a9fd18a30752583bdb4969db35b7
Mert18 <mertuygur02@gmail.com>
2021-07-07 00:10:40 +0300

    Requested configurations, help needed

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,15 +1,33 @@
-const { replies } = require('../../config.json');
-
+/* eslint-disable no-shadow */
+const { replies, emoji } = require('../../config.json');
 module.exports = {
 	name: 'play',
 	description: 'Plays the given song.',
-	execute: async ({ client, message, args }) => {
-		if (!message.member.voice.channel) return message.channel.send('🔴 **Please enter a voice channel to play a song.**');
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(replies.sameVC);
+	execute: async ({ Discord, client, message, args }) => {
 
-		const music = args.join(' ');
-		client.distube.play(message, music);
+		if(!message.member.voice.channel) {
+			message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+			message.react(emoji.x);
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Error')
+				.setDescription(replies.mustVC);
 
+			return message.channel.send(embed);
+		}
+		else if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+			message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+			message.react(emoji.x);
+
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Error')
+				.setDescription(replies.sameVC);
+
+			return message.channel.send(embed);
+		}
+		else {
+			const music = args.join(' ');
+			client.distube.play(message, music);
+		}
 	},
 };
 
--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,20 +1,44 @@
-const { replies } = require('../../config.json');
-
+const Discord = require('discord.js');
+const { emoji, replies } = require('../../config.json');
 module.exports = {
 	name: 'skip',
 	description: 'Skips the playing song.',
 	execute: async ({ client, message }) => {
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(replies.sameVC);
+		if(!message.member.voice.channel) {
+			message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+			message.react(emoji.x);
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Error')
+				.setDescription(replies.mustVC);
+
+			return message.channel.send(embed);
+		}
+		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+			message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+			message.react(emoji.x);
+
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Error')
+				.setDescription(replies.sameVC);
+
+			return message.channel.send(embed);
+		}
 
 		const queue = await client.distube.getQueue(message);
 
 		if(queue) {
 			client.distube.skip(message);
 
-			message.channel.send('▶ **The song has skipped.**');
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Skip')
+				.setDescription(`${emoji.checkmark} The song has skipped.`);
+			return message.channel.send(embed);
 		}
 		else if (!queue) {
-			return;
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Empty Queue')
+				.setDescription(`The queue is empty. If you want to stop the bot, use !stop instead.`);
+			return message.channel.send(embed);
 		}
 	},
 };
