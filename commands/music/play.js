1f3722d206af91a5cc4e0fc7e3a536f179a7a187
j-dogcoder <j.dogcoder@gmail.com>
2021-07-08 13:39:09 -0400

    :twisted_rightwards_arrows: merge part 2

--- commands/music/play.js
+++ commands/music/play.js
@@ -3,11 +3,8 @@ const { replies, emoji } = require('../../config.json');
 module.exports = {
 	name: 'play',
 	description: 'Plays the given song.',
-<<<<<<< HEAD
-=======
 	guildOnly: false,
 	category: 'Music',
->>>>>>> staged
 	execute: async ({ Discord, client, message, args }) => {
 
 		if(!message.member.voice.channel) {
