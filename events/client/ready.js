0ed17c6935f207fb6141674e70d1c805358207c3
j-dogcoder <j.dogcoder@gmail.com>
2021-07-13 14:47:17 -0400

    :wrench: Avatar auto set from cdn

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,11 +3,13 @@ const chalk = require('chalk');
 
 module.exports = async (Discord, client) => {
 
+	client.user.setAvatar('https://cdn.heptagram.xyz/HeptagramSquare.png');
+	client.user.setStatus('online');
+	client.user.setActivity(`${prefix}help`, { type: 'LISTENING' });
+
+
 	console.log(chalk.magenta('Starting Heptagram\nNode version: ' + process.version + '\nDiscord.js version: ' + Discord.version));
 	console.log(chalk.green(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`));
 
-	client.user.setStatus('online');
-	client.user.setActivity(`${prefix}help`, { type: 'LISTENING' })
-		.then(presence => console.log(chalk.cyanBright(`Activity set to ${presence.activities[0].name}.`)))
-		.catch(console.error);
+	console.log(chalk.cyanBright(`Activity set to ${client.presence.activities[0].name}.`));
 };
