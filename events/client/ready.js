fbe4eccb547af86ebee234c4a33910741c0013b6
j-dogcoder <j.dogcoder@gmail.com>
2021-07-14 09:42:35 -0400

    Changes ready message slightly, removes cdn for avatar due to rate limits

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,7 +3,6 @@ const chalk = require('chalk');
 
 module.exports = async (Discord, client) => {
 
-	client.user.setAvatar('https://cdn.heptagram.xyz/HeptagramSquare.png');
 	client.user.setStatus('online');
 	client.user.setActivity(`${prefix}help`, { type: 'LISTENING' });
 
@@ -11,5 +10,4 @@ module.exports = async (Discord, client) => {
 	console.log(chalk.magenta('Starting Heptagram\nNode version: ' + process.version + '\nDiscord.js version: ' + Discord.version));
 	console.log(chalk.green(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`));
 
-	console.log(chalk.cyanBright(`Activity set to ${client.presence.activities[0].name}.`));
 };
