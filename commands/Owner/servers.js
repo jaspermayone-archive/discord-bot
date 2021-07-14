e7509f2d4daf5040b8ead155282137f11558e837
j-dogcoder <j.dogcoder@gmail.com>
2021-07-14 14:48:03 -0400

    :boom: [BREAKING] Starts work on servers command args

--- commands/Owner/servers.js
+++ commands/Owner/servers.js
@@ -1,18 +1,25 @@
-const { IDs } = require('../../config.json');
+const { IDs, replies } = require('../../config.json');
 
 module.exports = {
 	name: 'servers',
 	description: 'gets list of servers bot is in.',
 	guildOnly: false,
 
-	execute({ client, message }) {
+	execute({ client, message, args }) {
 		if (message.author.id === (IDs.OwnerID)) {
-			client.guilds.cache.forEach((guild) => {
-				message.channel.send(`The server ${guild.name} (\`${guild.id}\`) has a total of ${guild.memberCount} members.`);
-			});
+
+			if (!args[0]) {
+				client.guilds.cache.forEach((guild) => {
+					message.channel.send(`The server ${guild.name} (\`${guild.id}\`) has a total of ${guild.memberCount} members.`);
+				});
+			}
+			else {
+				const serverArg = args[0];
+				console.log(serverArg);
+			}
 		}
 		else {
-			message.channel.send('Sorry, this command is resticted!');
+			message.channel.send(replies.restricted);
 		}
 	},
 };
