e768e494e8535b71c5b986aff2b1ae20b5449802
j-dogcoder <j.dogcoder@gmail.com>
2021-07-16 08:31:23 -0400

    :adhesive_bandage: small updates to example commands

--- commands/Examples/Admin.js
+++ commands/Examples/Admin.js
@@ -7,10 +7,9 @@ module.exports = {
 	execute({ message, roles }) {
 		if (message.member.roles.cache.has(roles.admin)) {
 			message.channel.send('This is an admin command example');
-			// The resricted reponse message goes in here.
 		}
 		else {
-			message.channel.send('Sorry, this command is resticted!');
+			message.channel.send('Sorry, this is a restricted command example!');
 		}
 	},
 };
