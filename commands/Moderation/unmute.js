5c4e9b3fb980b856cfd2a050e04850bc1baa83eb
j-dogcoder <j.dogcoder@gmail.com>
2021-07-21 17:53:16 -0400

    :passport_control: Perms for unmute not caught in last commit

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -10,20 +10,15 @@ module.exports = {
 
 	execute({ message, roles }) {
 
-		if (!message.member.hasPermission('MOVE_MEMBERS')) {
-			const target = message.mentions.users.first();
+		const target = message.mentions.users.first();
 
-			const mainRole = message.guild.roles.cache.get(roles.users);
-			const muteRole = message.guild.roles.cache.get(roles.muted);
+		const mainRole = message.guild.roles.cache.get(roles.users);
+		const muteRole = message.guild.roles.cache.get(roles.muted);
 
-			const memberTarget = message.guild.members.cache.get(target.id);
+		const memberTarget = message.guild.members.cache.get(target.id);
 
-			memberTarget.roles.remove(muteRole.id);
-			memberTarget.roles.add(mainRole.id);
-			message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
-		}
-		else {
-			message.reply(replies.restricted);
-		}
+		memberTarget.roles.remove(muteRole.id);
+		memberTarget.roles.add(mainRole.id);
+		message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
 	},
 };
