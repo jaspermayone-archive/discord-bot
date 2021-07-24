4ba393979ea4b891af0fb173d8e2e3b250d64b33
j-dogcoder <j.dogcoder@gmail.com>
2021-07-23 22:00:42 -0400

    :bug: Fixes uptime command not responding

--- commands/Resources and Utilitys/uptime.js
+++ commands/Resources and Utilitys/uptime.js
@@ -9,33 +9,35 @@ module.exports = {
 	maxArgs: 0,
 	expectedArgs: "",
 
-	run: async () => {
+	run: async (message) => {
 		try {
-		  const seconds = Math.round(process.uptime());
-		  const days = seconds >= 86400 ? Math.floor(seconds / 86400) : 0;
-		  const hours =
-			seconds >= 3600 ? Math.floor((seconds - days * 86400) / 3600) : 0;
-		  const minutes =
-			seconds >= 60
-			  ? Math.floor((seconds - days * 86400 - hours * 3600) / 60)
-			  : 0;
-		  const secondsRemain =
-			seconds - days * 86400 - hours * 3600 - minutes * 60;
+			const seconds = Math.round(process.uptime());
+			const days = seconds >= 86400 ? Math.floor(seconds / 86400) : 0;
+			const hours =
+				seconds >= 3600 ? Math.floor((seconds - days * 86400) / 3600) : 0;
+			const minutes =
+				seconds >= 60
+					? Math.floor((seconds - days * 86400 - hours * 3600) / 60)
+					: 0;
+			const secondsRemain =
+				seconds - days * 86400 - hours * 3600 - minutes * 60;
 
-		  const uptimeEmbed = new Discord.MessageEmbed();
-		  uptimeEmbed.setTitle("Heptagram Uptime");
-		  uptimeEmbed.setColor(colors.heptagram);
-		  uptimeEmbed.setDescription("Current Heptagram Uptime.");
-		  uptimeEmbed.addField("Days", days);
-		  uptimeEmbed.addField("Hours", hours, true);
-		  uptimeEmbed.addField("Minutes", minutes, true);
-		  uptimeEmbed.addField("Seconds", secondsRemain, true);
-		  uptimeEmbed.setTimestamp();
-		  uptimeEmbed.setFooter("Message sent by the Heptagram Bot", `${cdn.sqlogo}`);
+			const uptimeEmbed = new Discord.MessageEmbed()
+				.setTitle("Heptagram Uptime:")
+				.setColor(colors.heptagram)
+				.addFields(
+					{ name: 'Days', value: days, inline: true },
+					{ name: 'Hours', value: hours, inline: true },
+					{ name: 'Minutes', value: minutes, inline: true },
+					{ name: 'Seconds', value: secondsRemain, inline: true },
 
-		  return { success: true, content: uptimeEmbed };
+				)
+				.setTimestamp()
+				.setFooter("Message sent by the Heptagram Bot", `${cdn.sqlogo}`);
 
+			message.channel.send({ embed: uptimeEmbed });
+			return { success: true, content: uptimeEmbed };
 		}
-		catch (err) {}
+		catch (err) { }
 	},
 };
