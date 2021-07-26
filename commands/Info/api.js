4e8e710453f53e24edf625e3eeba0d6c495c7f06
j-dogcoder <j.dogcoder@gmail.com>
2021-07-26 16:11:42 -0400

    :sparkles: New api info commad

---
+++ commands/Info/api.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'api',
+	aliases: ['API'],
+	description: 'gives info about the Heptagram API',
+	category: 'Info',
+	minArgs: 0,
+	maxArgs: 0,
+	expectedArgs: "",
+
+	execute: ({ message }) => {
+		message.channel.send('The Heptagram team is curently working on developing an API. Stay tuned for more info.');
+	},
+};
