82d2e485abe6dfbbf20d7adacc11badc343407ba
j-dogcoder <j.dogcoder@gmail.com>
2021-07-28 10:14:32 -0400

    :sparkles: changes join commannd with various updates and upgrades

--- commands/Owner/join.js
+++ commands/Owner/join.js
@@ -1,12 +1,25 @@
+const { colors } = require('../../config.json');
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	name: 'join',
+	aliases: ['j', 'emmit', 'e'],
 	description: 'pretends a new user has joined (for testing only)',
 	category: 'Owner',
 	ownerOnly: true,
 	hidden: true,
 
 	execute({ client, message }) {
+
 		client.emit('guildMemberAdd', message.member);
-		message.channel.send('Join emmited!');
+
+		const embed = new MessageEmbed()
+			.setColor(colors.heptagram)
+			.setTitle(`Join Emmitted!`)
+			.setDescription(`You have succesfully emmited a join. || <@${message.author.id}>`)
+			.setTimestamp()
+			.setFooter("Message sent by the Heptagram Bot", 'https://cdn.heptagram.xyz/Logos/HeptagramLogo%28square%29.png');
+
+		message.channel.send(embed);
 	},
 };
