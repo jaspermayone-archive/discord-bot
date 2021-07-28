2179aa07e5adee2e152034fd0a4b585e7d261666
j-dogcoder <j.dogcoder@gmail.com>
2021-07-28 11:10:38 -0400

    :lipstick: Makes restart command an embed

--- commands/Owner/restart.js
+++ commands/Owner/restart.js
@@ -1,4 +1,4 @@
-const { colors, emoji } = require('../../config.json');
+const { colors } = require('../../config.json');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
@@ -13,23 +13,14 @@ module.exports = {
 
 		const embed = new MessageEmbed()
 			.setColor(colors.heptagram)
-			.setTitle(`${emoji.status_offline} **Bot Restarting!** ${emoji.status_offline}`)
+			.setTitle(`<:status_offline:852483939955769375> **Bot Restarting!** <:status_offline:852483939955769375>`)
 			.setDescription(`The bot has been qued to restart.`)
 			.setTimestamp()
 			.setFooter("Message sent by the Heptagram Bot", 'https://cdn.heptagram.xyz/Logos/HeptagramLogo%28square%29.png');
 
-		message.channel.send(embed);
-		client.destroy();
-
-		await client.restart().then(() => {
-			const restartembed = new MessageEmbed()
-				.setColor(colors.heptagram)
-				.setTitle(`${emoji.status_online} **Bot Restarted** ${emoji.status_online}`)
-				.setDescription(`The bot has restarted.`)
-				.setTimestamp()
-				.setFooter("Message sent by the Heptagram Bot", 'https://cdn.heptagram.xyz/Logos/HeptagramLogo%28square%29.png');
-
-			message.channel.send(restartembed);
+		await message.channel.send(embed).then(() => {
+			client.destroy();
+			process.exit();
 		});
 	},
 };
