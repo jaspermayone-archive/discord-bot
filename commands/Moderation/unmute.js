d536bb5d5edc32f1687dd5c2f04f865b80fc89c0
j-dogcoder <j.dogcoder@gmail.com>
2021-08-01 10:31:57 -0400

    :lipstick: Changes unmute command to an embed

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -1,3 +1,6 @@
+const { MessageEmbed } = require('discord.js');
+const { colors, roles } = require('../../config.json');
+
 module.exports = {
 	name: 'unmute',
 	guildOnly: true,
@@ -8,7 +11,7 @@ module.exports = {
 	expectedArgs: "<@user you want to unmute>",
 	permissions: ["MUTE_MEMBERS"],
 
-	execute({ message, roles }) {
+	execute({ message }) {
 
 		const target = message.mentions.users.first();
 
@@ -18,7 +21,17 @@ module.exports = {
 		const memberTarget = message.guild.members.cache.get(target.id);
 
 		memberTarget.roles.remove(muteRole.id);
-		memberTarget.roles.add(mainRole.id);
-		message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+		memberTarget.roles.add(mainRole.id).then(() => {
+
+			const embed = new MessageEmbed()
+				.setColor(colors.heptagram)
+				.setTitle(`:white_check_mark: **Success!** :white_check_mark:`)
+				.setDescription(`You have succesfully muted <@${memberTarget.user.id}>`)
+				.setTimestamp()
+				.setFooter("Message sent by the Heptagram Bot", 'https://cdn.heptagram.xyz/Logos/HeptagramLogo%28square%29.png');
+
+			message.channel.send(embed);
+
+		});
 	},
 };
