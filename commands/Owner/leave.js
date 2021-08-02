e897f3be572b00df43055071df8b800d34b6af8c
j-dogcoder <j.dogcoder@gmail.com>
2021-08-02 12:34:12 -0400

    :sparkles: Adds leave emmit.

--- commands/Owner/leave.js
+++ commands/Owner/leave.js
@@ -2,21 +2,21 @@ const { colors, cdn } = require('../../config.json');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	name: 'join',
-	aliases: ['ej'],
-	description: 'pretends a new user has joined (for testing only)',
+	name: 'leave',
+	aliases: ['el'],
+	description: 'pretends a user has left (for testing only)',
 	category: 'Owner',
 	ownerOnly: true,
 	hidden: true,
 
 	execute({ client, message }) {
 
-		client.emit('guildMemberAdd', message.member);
+		client.emit('guildMemberLeave', message.member);
 
 		const embed = new MessageEmbed()
 			.setColor(colors.heptagram)
 			.setTitle(`Join Emmitted!`)
-			.setDescription(`You have succesfully emmited a join. || <@${message.author.id}>`)
+			.setDescription(`You have succesfully emmited a leave. || <@${message.author.id}>`)
 			.setTimestamp()
 			.setFooter("Message sent by the Heptagram Bot", `${cdn.sqlogo}`);
 
