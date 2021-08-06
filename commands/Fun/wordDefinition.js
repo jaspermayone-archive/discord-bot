3aebb831e2c684e2326a2a4acd30f88614ceb5a0
Pomf <38390458+pomf1@users.noreply.github.com>
2021-08-06 20:06:10 -0300

    Removed language support.

--- commands/Fun/wordDefinition.js
+++ commands/Fun/wordDefinition.js
@@ -1,44 +1,20 @@
 const fetch = require('node-fetch');
 
-const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries';
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en_US';
 
 module.exports = {
 	name: 'word',
 	cooldown: '1m',
 	description: 'Shows word definitions',
 	category: 'Fun',
-	minArgs: 2,
-	maxArgs: 2,
-	expectedArgs: "<word> <lang code>",
+	minArgs: 1,
+	maxArgs: 1,
+	expectedArgs: "<word>",
 
 	execute: async (message, args) => {
-		const AVAILABLE_LANGUAGES = [
-			'en_US',
-			'hi',
-			'es',
-			'fr',
-			'ja',
-			'ru',
-			'en_GB',
-			'de',
-			'it',
-			'ko',
-			'pt-BR',
-			'ar',
-			'tr',
-		];
 		const word = args[0].toLowerCase();
-		const lang = args[1];
 
-		if (!AVAILABLE_LANGUAGES.includes(lang)) {
-			return message.channel.send(
-				`❌ | Language not available.\n\nUse one of the following: \n${AVAILABLE_LANGUAGES.map(
-					(language) => `\`${language}\``
-				).join(' ')}`
-			);
-		}
-
-		const response = await fetch(`${API_URL}/${lang}/${word}`);
+		const response = await fetch(`${API_URL}/${word}`);
 		const data = await response.json();
 
 		if (data.title === 'No Definitions Found') {
