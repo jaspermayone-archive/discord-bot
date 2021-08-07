ce611d7bb97c4db75588f92225ee1725dbb9a6f7
j-dogcoder <j.dogcoder@gmail.com>
2021-08-07 09:34:44 -0400

    :sparkles: Minor update to user command

--- commands/Resources and Utilitys/user.js
+++ commands/Resources and Utilitys/user.js
@@ -23,19 +23,21 @@ module.exports = {
 
 		const member = message.guild.member(user);
 
+
 		const embed = new Discord.MessageEmbed()
 			.setColor(colors.heptagram)
 			.setThumbnail(message.author.avatarURL)
 			.addField(`${user.tag}`, `${user}`, true)
 			.addField('ID:', `${user.id}`, true)
 			.addField('Nickname:', `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+			.addField("Colour", user.displayHexColor, true)
 			.addField('Status:', `${user.presence.status}`, true)
 			.addField('In Server', message.guild.name, true)
 			.addField('Game:', `${user.presence.game ? user.presence.game.name : 'None'}`, true)
 			.addField('Bot:', `${user.bot}`, true)
 			.addField('Joined The Server On:', `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY')}`, true)
 			.addField('Account Created On:', `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY')}`, true)
-			.addField("Nitro", member.premiumSinceTimestamp ? `Since ${new Date(member.premiumSinceTimestamp).toLocaleDateString()}` : "No.", false)
+			.addField("Nitro", user.premiumSinceTimestamp ? `Since ${new Date(user.premiumSinceTimestamp).toLocaleDateString()}` : "No.", true)
 			.addField('Roles:', (member.roles.cache.map((role) => `<@&${role.id}>`).join(" ")), false)
 			.setTimestamp()
 			.setFooter("Message sent by the Heptagram Bot", `${cdn.sqlogo}`);
