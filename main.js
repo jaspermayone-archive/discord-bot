7bb6abf5be5fb9208adc042be1e1e6bfb23c0087
Panquesito7 <halfpacho@gmail.com>
2021-08-10 18:22:13 +0000

    feat: Add welcome/leave messages

--- main.js
+++ main.js
@@ -7,8 +7,32 @@ const { token, colors, MongoDB, IDs, emoji } = require('./config.json');
 const antiAd = require('./features/anti-link');
 const antiInvite = require('./features/anti-invite');
 
-
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
+// Welcome messages. Requires a channel named `#welcome`.
+client.on("guildMemberAdd", async member => {
+	if (member.id === bot.user.id) return // If a bot was added to a server, do not send a welcome message.
+	let welcomemsgs = [
+	  `Welcome, ${member}, to the Heptagram Discord server. If you need any help, head over to the <#826519674640269373> channel. :wave: <:HeptagramLogo:874265504813056020>`,
+	  `Hi there, ${member}! We're glad to have you here! :smile:`,
+	  `${member} has just joined us! Say hello in the <#869607626185527366> channel! :grin:`
+	]
+
+	let msg = Math.floor(Math.random() * welcomemsgs.length)
+	getChannel(member.guild, "welcome")
+	  .send(welcomemsgs[msg].toString())
+})
+
+// Leaving messages. Requires a channel named `#leaves`.
+client.on("guildMemberRemove", member => {
+	if (member.id === bot.user.id) return  // If a bot was removed from a server, do not send a leave message.
+	let leavemsgs = [
+	  `Goodbye, **${member.user.tag}**. We hope you come back. :wink:`,
+	  `${member.user.tag} just left the server. :wave:`,
+	]
+	let msg = Math.floor(Math.random() * leavemsgs.length)
+	getChannel(member.guild, "leaves").typeMessage(leavemsgs[msg])
+})
 
 client.distube = new distube(client, { searchSongs: false, emitNewSongOnly: true });
 client.distube
