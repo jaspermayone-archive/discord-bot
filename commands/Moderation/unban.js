0dc5aa12a6e7eae255e8d8819f218ec61c259500
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-10-01 08:48:53 +0530

    Added unban/eval/anime commands

---
+++ commands/Moderation/unban.js
@@ -0,0 +1,57 @@
+const { MessageEmbed } = require('discord-js');
+const { colors, cdn, emoji } = require('../../config.json');
+const pjson = require('../../package.json');
+
+module.exports = {
+	name: 'unban',
+	guildOnly: true,
+	description: 'unbans users',
+	category: 'Moderation',
+	minArgs: 2,
+	maxArgs: -1,
+	expectedArgs: "<@member you want to kick> <reason>",
+	permissions: ["BAN_MEMBERS"],
+
+	callback: async ({ message, client, args }) => {
+                 
+		
+		if (!args[0]) return message.channel.send('please enter a users id to unban!').then(m => m.delete({ timeout: 5000 }));
+
+		let member;
+	    
+		try {
+		    member = await client.users.fetch(args[0])
+		} catch (e) {
+		    console.log(e)
+		    return message.channel.send('Not a valid user!').then(m => m.delete({ timeout: 5000 }));
+		}
+	    
+		const reason = args[1] ? args.slice(1).join(' ') : 'no reason';
+	    
+		const embed = new MessageEmbed()
+		    .setFooter(`${message.author.tag} | ${message.author.id}`, message.author.displayAvatarURL({ dynamic: true }));
+	    
+		message.guild.fetchBans().then( bans => {
+	    
+		    const user = bans.find(ban => ban.user.id === member.id );
+	    
+		    if (user) {
+			embed.setTitle(`Successfully Unbanned ${user.user.tag}`)
+			    .setColor('#9b6dff')
+			    .addField('User ID', user.user.id, true)
+			    .addField('user Tag', user.user.tag, true)
+			    .addField('Banned Reason', user.reason != null ? user.reason : 'no reason')
+			    .addField('Unbanned Reason', reason)
+			    .setColor("#9b6dff")
+			message.guild.members.unban(user.user.id, reason).then(() => message.channel.send(embed))
+		    } else {
+			embed.setTitle(`User ${member.tag} isn't banned!`)
+			    .setColor('#ff0000')
+			message.channel.send(embed)
+		    }
+	    
+		}).catch(e => {
+		    console.log(e)
+		    message.channel.send('An error has occurred!')
+		})
+	}}
