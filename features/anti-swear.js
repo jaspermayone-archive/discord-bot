de33573267a09a94ae566c6e1bc130a4b9b38904
j-dogcoder <j.dogcoder@gmail.com>
2021-10-04 14:57:10 -0400

    :recycle: More cleanup.

--- features/anti-swear.js
+++ features/anti-swear.js
@@ -1,17 +1,18 @@
 const filter = require('leo-profanity');
-const Permissions = require('discord.js').Permissions;
 
 module.exports = async function (message) {
-  if (message.channel.type === 'DM') return;
+  if (message.guild == null) return;
   if (message.deleted) return;
-  if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
-    // Should be made configurable soon
-    if (filter.check(message.content)) {
-      message.delete();
-      const warning = await message.channel.send(
-        `${message.author}, do not use bad words in chat!`,
-      );
-      setTimeout(warning.delete, 4500);
-    }
+  if (filter.check(message.content)) {
+    message.delete();
+    const warning = await message.channel.send(
+      `${message.author}, do not use bad words in chat!`,
+    );
+    setTimeout(warning.delete, 4500);
   }
+};
+
+module.exports.config = {
+  displayName: 'Anti Sewar',
+  dbName: 'ANTI SWEAR',
 };
