622f159ce556b738d883da9b4132e348c4bb3e85
monzilnepali <manjilnepali98@gmail.com>
2021-10-08 12:56:26 +0545

    Fix issue of missing message body on annoucement embed

--- commands/Resources and Utilitys/announce.js
+++ commands/Resources and Utilitys/announce.js
@@ -8,37 +8,37 @@ module.exports = {
   description: 'Make an Announcemnet in your Server',
   category: 'Resources',
   minArgs: 2,
-  maxArgs: 2,
   expectedArgs: '<channel> <msg>',
   permissions: ['MANAGE_MESSAGES'],
   cooldown: '1m',
 
   callback: ({ message, args, client }) => {
-    message.reply(
-      'This command is curently under reconstuction, and it doesn\t quite work yet.',
-    );
+    const anchannel = message.mentions.channels.first();
 
-    /* 	const anchannel = message.mentions.channels.first();
+    const announcementMessage = args.slice(1).join(' ');
+    if (!announcementMessage) {
+      return message.reply('');
+    }
 
-		if (!args.slice(1).join(' ')) {
-			return message.reply('');
-		}
+    const embed = new Discord.MessageEmbed()
+      .setTitle('**Announcement!**')
+      .setColor(colors.heptagram)
+      .setDescription(announcementMessage)
+      .setTimestamp()
+      .setFooter('Message sent by the Heptagram Bot', `${cdn.sqlogo}`);
 
-		const embed = new Discord.MessageEmbed()
-			.setTitle('**Announcement!**')
-			.setColor(colors.heptagram)
-			.setTimestamp()
-			.setFooter("Message sent by the Heptagram Bot", `${cdn.sqlogo}`);
+    anchannel.send({ embeds: [embed] });
 
-		anchannel.send({ embeds: [embed] });
+    const anembed = new Discord.MessageEmbed()
+      .setTitle('Done!')
+      .setDescription(`Announcement has been sent to ${anchannel}`)
+      .setColor(colors.heptagram)
+      .setTimestamp()
+      .setFooter(
+        `Message sent by the Heptagram Bot || ${pjson.version}`,
+        `${cdn.sqlogo}`,
+      );
 
-		const anembed = new Discord.MessageEmbed()
-			.setTitle('Done!')
-			.setDescription(`Announcement has been sent to ${anchannel}`)
-			.setColor(colors.heptagram)
-			.setTimestamp()
-			.setFooter(`Message sent by the Heptagram Bot || ${pjson.version}`, `${cdn.sqlogo}`);
-
-		message.reply({ embeds: [anembed] }); */
+    message.reply({ embeds: [anembed] });
   },
 };
