06a7c1fb5a1ed1ebc9dfb9a766488d64a7eb8091
Jasper Mayone <jasper@jaspermayone.com>
2022-04-17 21:29:03 -0400

    :ambulance: Bizzare hotfixes

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,4 +1,4 @@
-const chalk = require ("chalk");
+const chalk = require("chalk");
 const { Discord } = require("discord.js");
 const { Timestamp } = require ("@sapphire/time-utilities");
 const fs = require("fs");
