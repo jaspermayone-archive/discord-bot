50e3a44d3019cc32c08d83a2d2ca292db0ad13cc
Jasper Mayone <jasper@jaspermayone.com>
2022-08-29 18:43:29 -0400

    :bug: Bugfixes

--- src/modules/heptagramLogger.ts
+++ src/modules/heptagramLogger.ts
@@ -1,64 +1,39 @@
-import chalk from "chalk";
+import colors from "colors";
 import moment from "moment";
 
 export const log = (
   content: string,
-  type:
-    | "log"
-    | "info"
-    | "warn"
-    | "error"
-    | "debug"
-    | "load"
-    | "ready"
-    | "mongo"
-    | "heptagram" = "log"
+  type: "log" | "info" | "warn" | "error" | "debug" | "load" | "ready" = "log"
 ) => {
-  const timestamp = `${chalk.white(`[${moment().format("DD-MM-YY H:m:s")}]`)}`;
+  const timestamp = `${colors.white(`[${moment().format("DD-MM-YY H:m:s")}]`)}`;
 
   switch (type) {
     case "log":
-      return console.log(`${chalk.grey("[LOG]")} ${timestamp} ${content}`);
+      return console.log(`${colors.grey("[LOG]")} ${timestamp} ${content}`);
     case "info":
       return console.log(
-        `${chalk.blueBright("[INFO]")} ${timestamp} ${chalk.blueBright(
-          content
-        )}`
+        `${colors.cyan("[INFO]")} ${timestamp} ${colors.cyan(content)}`
       );
     case "warn":
       return console.log(
-        `${chalk.yellow("[WARN]")} ${timestamp} ${chalk.yellow(content)} `
+        `${colors.yellow("[WARN]")} ${timestamp} ${colors.yellow(content)} `
       );
     case "error":
       return console.log(
-        `${chalk.red("[ERROR]")} ${timestamp} ${chalk.red(content)} `
+        `${colors.red("[ERROR]")} ${timestamp} ${colors.red(content)} `
       );
     case "debug":
       return console.log(
-        `${chalk.blueBright("[DEBUG]")}  ${timestamp} ${chalk.magenta(
-          content
-        )} `
+        `${colors.magenta("[DEBUG]")}  ${timestamp} ${colors.magenta(content)} `
       );
     case "load": {
       return console.log(
-        `${timestamp} ${chalk.magenta(type.toUpperCase())} ${content} `
+        `${timestamp} ${colors.magenta(type.toUpperCase())} ${content} `
       );
     }
     case "ready":
       return console.log(
-        `${chalk.green("[READY]")}  ${timestamp} ${chalk.green(content)}`
-      );
-    case "mongo":
-      return console.log(
-        `${chalk.hex("#f1421a")("[MONGO]")} ${timestamp} ${chalk.hex("#f1421a")(
-          content
-        )}`
-      );
-    case "heptagram":
-      return console.log(
-        `${chalk.hex("#FFF800")("[HEPTAGRAM]")}  ${timestamp}  ${chalk.hex(
-          "#FFF800"
-        )(`${content}`)}`
+        `${colors.green("[READY]")}  ${timestamp} ${colors.green(content)}`
       );
     default:
       throw new TypeError("Logger type not correct.");
@@ -71,5 +46,3 @@ export const error = (content: string) => log(content, "error");
 export const debug = (content: string) => log(content, "debug");
 export const load = (content: string) => log(content, "load");
 export const ready = (content: string) => log(content, "ready");
-export const mongo = (content: string) => log(content, "mongo");
-export const heptagram = (content: string) => log(content, "heptagram");
